c61b6c9e627bff55096ed66a6b8f58fe3993eb42
Kokoluc7 <viky7u7.5@gmail.com>
2021-09-09 12:55:17 -0700

    publish

---
+++ src/main.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "Something.hh"
+
+int main()
+{
+    int number{5};
+
+    std::cout << SayHello ("Pancho")<< std::endl;
+    dosomething();
+    std::cout << Add(1, 1) << std::endl;
+    PrintAdd(2, 3);
+    std::cout << number << std::endl;
+    AddOneByRef(number);
+    std::cout << number << std::endl;
+    return 0;
+}
